1150ecb1319d396705ebd3f09a8c7a6b27db979a
Aaraav <aaraav2810@gmail.com>
2024-07-08 01:27:17 +0530

    done

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const indexRouter = require('./views/server');
 const app = express();
 
 const corsOptions = {
-    origin: 'https://accredian-frontend-task-ashy.vercel.app',
-    optionsSuccessStatus: 200 
+    origin: 'https://accredian-frontend-task-ashy.vercel.app', // Your frontend origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    optionsSuccessStatus: 204
   };
+  
+  app.use(cors(corsOptions));
+  app.options('*', cors(corsOptions));
 app.use(express.json()); 
 app.use('/api/user', indexRouter); 
 
